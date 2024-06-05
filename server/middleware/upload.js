f6ff48ac49bafbfa8100e3625964a51f1f736926
unknown <zainshahid989@gmail.com>
2024-06-05 14:22:04 +0500

    Added vercel.jso

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -4,10 +4,27 @@
 
 // module.exports = upload;
 
-const multer = require("multer");
+// const multer = require("multer");
 
-// Configure Multer to use memory storage
-const storage = multer.memoryStorage();
+// // Configure Multer to use memory storage
+// const storage = multer.memoryStorage();
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+
+const multer = require("multer");
+const path = require("path");
+
+// Configure Multer to use the /tmp directory
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
 const upload = multer({ storage: storage });
 
